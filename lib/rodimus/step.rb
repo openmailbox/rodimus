b7361c21b89ff7fdcf4ec6a61aa7758240836639
Brandon Rice <brice84@gmail.com>
2014-06-07 08:38:17 -0400

    First crude benchmarking attempt.

--- lib/rodimus/step.rb
+++ lib/rodimus/step.rb
@@ -11,6 +11,8 @@ module Rodimus
     # This is initialized by the Transformation when the step begins to run.
     attr_accessor :shared_data
 
+    attr_reader :benchmark
+
     def close_descriptors
       [incoming, outgoing].reject(&:nil?).each do |descriptor|
         descriptor.close if descriptor.respond_to?(:close)
@@ -31,16 +33,31 @@ module Rodimus
     end
 
     def run
+      start_time = Time.now.to_i
+      @benchmark = {count: 0, total: 0, min: 100, max: 0, average: 0}
       Rodimus.logger.info "Running #{self}"
       @row_count = 1
       incoming.each do |row|
+        benchmark[:count] += 1
+        row_start_time = Time.now.to_f
         transformed_row = process_row(row)
         handle_output(transformed_row)
         Rodimus.logger.info(self) { "#{@row_count} rows processed" } if @row_count % 50000 == 0
         @row_count += 1
+        row_run_time = (Time.now.to_f - row_start_time).round(4)
+        benchmark[:total] = (benchmark[:total] + row_run_time).round(4)
+        benchmark[:min] = row_run_time if benchmark[:min] > row_run_time
+        benchmark[:max] = row_run_time if benchmark[:max] < row_run_time
       end
+      benchmark[:average] = (benchmark[:total] / benchmark[:count]).round(4)
       finalize
-      Rodimus.logger.info "Finished #{self}"
+      run_time = Time.now.to_i - start_time
+      elapsed_hours = run_time / 3600
+      remaining_seconds = run_time % 3600
+      elapsed_minuntes = remaining_seconds / 60
+      elapsed_seconds = remaining_seconds % 60
+      Rodimus.logger.info "Finished #{self} after #{elapsed_hours} hours, #{elapsed_minuntes} minutes, #{elapsed_seconds} seconds."
+      Rodimus.logger.info "#{self} benchmarks: #{benchmark}"
     ensure
       close_descriptors
     end
