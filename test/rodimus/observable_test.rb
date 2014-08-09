175f26ed12b69257fa738010256ba8f7120b6d94
Brandon Rice <brice84@gmail.com>
2014-08-08 22:01:59 -0400

    Add a test for the Observable module.

---
+++ test/rodimus/observable_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'rodimus'
+
+class TestObservable < MiniTest::Unit::TestCase
+  def setup
+    @fake_observer = Class.new do
+      attr_reader :subject, :event_type
+
+      def on_notify(subject, event_type)
+        @subject = subject
+        @event_type = event_type
+      end
+    end.new
+    @subject = Object.new.extend(Rodimus::Observable)
+    @subject.observers << @fake_observer
+  end
+
+  def test_observer_notification
+    @subject.notify(@subject, :test_event)
+    assert_equal @fake_observer.subject, @subject
+    assert_equal @fake_observer.event_type, :test_event
+  end
+end
